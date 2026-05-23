49e67c679e8f2c4994ac54e155fc51c0b712cb47
Martin <mrtncsto@gmail.com>
2026-05-23 11:20:39 -0400

    alessandra y fernando desde el stash proveniente de ramota

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-alessandra_llego_tarde = True
+alessandra_llego_tarde = False
 
 if alessandra_llego_tarde:
     print("Y no es la primera vez")
@@ -15,3 +15,10 @@ else: print("D:")
 eudis_esta_en_silencio = True
 if eudis_esta_en_silencio: print("Esta pensando")
 else: print("Silencio")
+
+fernando_hizo_la_tarea_tarde = True
+fernando_estaba_trabajando = True
+
+if fernando_hizo_la_tarea_tarde and fernando_estaba_trabajando:
+    print("Sospechoso")
+else: print("Sospechoso x2")
